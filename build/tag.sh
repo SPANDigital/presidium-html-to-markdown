8ab1e43486f2fcc2f36f9c7c379847f39b6ce0b4
meyerhp <helgard.meyer@spandigital.com>
2023-09-11 15:39:03 +0200

    Update branch ref in tag.sh

--- build/tag.sh
+++ build/tag.sh
@@ -2,15 +2,25 @@
 
 set -e
 
-git checkout "${TRAVIS_BRANCH}"
+DIR="$(dirname "$0")"
+source "${DIR}"/include.sh
 
-f_info_log "Calculating tag ${TRAVIS_BRANCH} branch..."
-if [ "${TRAVIS_BRANCH}" = "main" ]; then
+# Determine the current branch in GitHub Actions
+current_branch="${GITHUB_REF#refs/heads/}"
+
+f_info_log "Calculating tag ${current_branch} branch..."
+# Calculate the tag based on the branch
+if [ "$current_branch" = "main" ]; then
   tag=v$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.4-debian.9-x64-5.0 /repo -output json -showvariable MajorMinorPatch)
-elif [ "${TRAVIS_BRANCH}" = "develop" ]; then
+elif [ "$current_branch" = "develop" ]; then
   tag=v$(docker run --rm -v "$(pwd):/repo" gittools/gitversion:5.6.4-debian.9-x64-5.0 /repo -output json -showvariable SemVer)
+else
+  echo "Branch '$current_branch' is not configured for tagging."
+  exit 1
 fi
-f_info_log "The tag for current source code is: ${tag}"
+
+echo "The tag for the current source code is: $tag"
+
 
 # Create tag in github
 f_info_log "Pushing tag to github..."
